e433e97ddc4a15477223cbfe12534d4267c5b0de
Ahsan Gulzar <ahsangulzar009@gmail.com>
2025-03-19 19:45:41 +0500

    change on 7:25pm 19 march

--- src/componenets/pages/Trading/Trading.jsx
+++ src/componenets/pages/Trading/Trading.jsx
@@ -11,7 +11,7 @@ const Trading = () => {
       <div className='bg-[#212121]'>
          <section className='flex h-screen '>
 
-            {/* Sidebar */}
+            {/*left Sidebar */}
             <aside
                className='bg-[#292a2d] flex flex-1 flex-col items-center justify-center px-4 pb-8 relative'
             >
@@ -25,6 +25,8 @@ const Trading = () => {
                   />
                   <img src={chat} alt="Chat" className='size-10 cursor-pointer' />
                </div>
+
+               {/* Right Side */}
                {
                   messages.length === 0 ? (
                      <>
@@ -33,6 +35,42 @@ const Trading = () => {
                               Invest in Crypto Currency
                            </p>
                         </div>
+                        <form
+                           className="rounded-2xl border border-white relative top-5 p-4 md:w-[400px] flex items-center justify-center flex-col gap-3"
+                        >
+
+                           <div className="rounded-xl p-2 border border-white flex items-center justify-between gap-2 w-full">
+                              <p className='font-[Lato] md:text-2xl text-xl text-white w-[35%]'>Investment: </p>
+                              <input
+                                 type="text"
+                                 placeholder='Investment in $... '
+                                 className='bg-[#505050] text-[#FFFFFF] md:text-xl p-1 rounded-md h-8 w-[65%] outline-none'
+                              />
+                           </div>
+                           <div className="rounded-xl p-2 border border-white flex items-center justify-between gap-2">
+                              <p className='font-[Lato] md:text-2xl text-xl text-white md:w-[45%] w-1/2'>Investment Risk: </p>
+                              <input
+                                 type="text"
+                                 placeholder='% Risk...'
+                                 className='bg-[#505050] text-[#FFFFFF] md:text-xl p-1 rounded-md h-8 md:w-[55%] w-1/2 outline-none'
+                              />
+                           </div>
+                           <div className='flex items-center justify-between md:gap-65 gap-40 w-full'>
+                              <aside className='flex flex-col text-white gap-2 bg-[#505050] w-[70%]'>
+                                 <div className='flex items-center gap-2'>
+                                    <input type="radio" name="future" id="future" />
+                                    <label htmlFor="future" className='text-xl'>Future</label>
+                                 </div>
+                                 <div className='flex items-center gap-2'>
+                                    <input type="radio" name="spot" id="spot" />
+                                    <label htmlFor="spot" className='text-xl'>Spot</label>
+                                 </div>
+                              </aside>
+                              <button type='submit'>
+                                 Send
+                              </button>
+                           </div>
+                        </form>
                      </>
                   ) : (
                      <div></div>
