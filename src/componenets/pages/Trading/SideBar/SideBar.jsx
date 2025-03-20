1def37153ad2536650fd4d7550f7a84d0d93ee5c
Ahsan Gulzar <ahsangulzar009@gmail.com>
2025-03-20 17:48:05 +0500

    changes in 5:58pm 20/25

--- src/componenets/pages/Trading/SideBar/SideBar.jsx
+++ src/componenets/pages/Trading/SideBar/SideBar.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
+import chat_icon from '../../../../assets/chat.png'
+import { HiOutlineChatBubbleLeftRight } from "react-icons/hi2";
 
 
 const SideBar = ({ expand, setExpand }) => {
    return (
       <>
-         
+         <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : ' w-0 max-md:overflow-hidden'}`}>
 
-         {
+            <div className='ml-1'>
+               <button className={`mt-15 flex items-center text-xl justify-center cursor-pointer ${expand ? 'bg-[#5BB0FF] text-white hover:opacity-90 rounded-xl gap-2 p-1 px-3 w-max' : 'group relative size-5 mx-auto rounded-lg'}`}>
 
-         }
+                  {
+                     expand ? (
+                        <span className='text-3xl'>+</span>
+                     ) : (
+                        <HiOutlineChatBubbleLeftRight className='size-10 absolute left-[12px] text-white hidden md:block' />
+
+                     )
+                  }
+                  <div
+                     className='absolute w-max -top-2 left-15 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none
+                  '>
+                     New Chat
+                  </div>
+                  {expand && <p className='text-white font-medium'>New Chat</p>}
+               </button>
+
+               <div className={`mt-8 text-white text-sm ${expand ? 'block' : 'hidden'}`}>
+                  <p className='my-1 text-white text-xl'>Recents</p>
+                  {/* Chat Label */}
+               </div>
+            </div>
+
+
+         </div>
       </>
 
    )
--- src/componenets/pages/Trading/Trading.jsx
+++ src/componenets/pages/Trading/Trading.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import menu from '../../../assets/menu.png'
 import chat from '../../../assets/chat.png'
+import star_icon from '../../../assets/star_group 1.png'
+import profile_icon from '../../../assets/profile_icon 1.png'
+import SideBar from './SideBar/SideBar'
 
 const Trading = () => {
    const [expand, setExpand] = useState(false)
@@ -11,19 +14,37 @@ const Trading = () => {
       <div className='bg-[#212121]'>
          <section className='flex h-screen '>
 
-            {/*left Sidebar */}
+            <SideBar expand={expand} setExpand={setExpand} />
             <aside
                className='bg-[#292a2d] flex flex-1 flex-col items-center justify-center px-4 pb-8 relative'
             >
 
-               <div className='md:hidden absolute px-4 top-5 flex items-center justify-between w-full'>
-                  <img
-                     src={menu} alt="Menu"
-                     className='h-5 w-7 cursor-pointer'
-                     onClick={() => (expand ? setExpand(false) : setExpand(true))}
-                  // onClick={() => setExpand(prev => !prev)}
-                  />
-                  <img src={chat} alt="Chat" className='size-10 cursor-pointer' />
+               <div className=' absolute px-4 top-5 flex items-center justify-between w-full'>
+                  <div
+                     onClick={() => setExpand(!expand)}
+                     className='group relative flex items-center justify-center transition-all duration-300 size-9 cursor-pointer'
+                  >
+                     <img
+                        src={menu} alt="Menu"
+                        className={`h-5 w-7 cursor-pointer z-50 absolute ${expand ? 'md:right-64' : ''}`}
+                        
+                     />
+                     <div className={`absolute w-max ${expand ? 'right-[57px] top-[3px] -translate-x-1/2 z-50' : 'left-12'} opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}>
+                        {expand ? 'Close sidebar' : 'Open sidebar'}
+                        <div className="size-3 absolute bg-black rotate-45 top-1/2 -left-2 -translate-y-1/2"></div>
+
+                     </div>
+                  </div>
+                  <div className='font-[Michroma] text-md md:text-2xl flex items-center'>
+                     <h2 className='text-[#5BB0FF]'>Fin</h2>
+                     <h2 className='text-[#FFFFFF]'>Prompt</h2>
+                  </div>
+                  <img src={chat} alt="Chat" className='size-10 cursor-pointer md:hidden' />
+                  <div className='hidden md:flex items-center justify-center gap-3'>
+                     <p className='text-xl text-white'>Hi, ahsan@gmail</p>
+                     <img src={profile_icon} alt="Profile" className='size-13' />
+                  </div>
+
                </div>
 
                {/* Right Side */}
@@ -36,15 +57,15 @@ const Trading = () => {
                            </p>
                         </div>
                         <form
-                           className="rounded-2xl border border-white relative top-5 p-4 md:w-[400px] flex items-center justify-center flex-col gap-3"
+                           className="rounded-2xl border border-white relative top-5 px-5 py-7 md:w-[450px] flex items-start justify-center flex-col gap-3"
                         >
 
                            <div className="rounded-xl p-2 border border-white flex items-center justify-between gap-2 w-full">
-                              <p className='font-[Lato] md:text-2xl text-xl text-white w-[35%]'>Investment: </p>
+                              <p className='font-[Lato] md:text-2xl text-xl text-white w-[40%]'>Investment: </p>
                               <input
                                  type="text"
                                  placeholder='Investment in $... '
-                                 className='bg-[#505050] text-[#FFFFFF] md:text-xl p-1 rounded-md h-8 w-[65%] outline-none'
+                                 className='bg-[#505050] text-[#FFFFFF] md:text-xl p-1 rounded-md h-8 w-[60%] outline-none'
                               />
                            </div>
                            <div className="rounded-xl p-2 border border-white flex items-center justify-between gap-2">
@@ -55,19 +76,20 @@ const Trading = () => {
                                  className='bg-[#505050] text-[#FFFFFF] md:text-xl p-1 rounded-md h-8 md:w-[55%] w-1/2 outline-none'
                               />
                            </div>
-                           <div className='flex items-center justify-between md:gap-65 gap-40 w-full'>
-                              <aside className='flex flex-col text-white gap-2 bg-[#505050] w-[70%]'>
+                           <div className='flex items-center justify-center md:gap-30 gap-10 w-full '>
+                              <aside className='flex flex-col text-white gap-1 bg-[#505050] px-4 w-[60%] rounded-xl text-lg md:text-2xl'>
                                  <div className='flex items-center gap-2'>
                                     <input type="radio" name="future" id="future" />
-                                    <label htmlFor="future" className='text-xl'>Future</label>
+                                    <label htmlFor="future" className='cursor-pointer'>Future</label>
                                  </div>
                                  <div className='flex items-center gap-2'>
                                     <input type="radio" name="spot" id="spot" />
-                                    <label htmlFor="spot" className='text-xl'>Spot</label>
+                                    <label htmlFor="spot" className='cursor-pointer'>Spot</label>
                                  </div>
                               </aside>
-                              <button type='submit'>
+                              <button type='submit' className='w-[40%] bg-[#5BB0FF] h-12 md:h-12 flex items-center justify-center px-5 text-white text-xl md:text-2xl gap-2 rounded-3xl cursor-pointer'>
                                  Send
+                                 <img src={star_icon} alt="" className='size-7 md:size-8' />
                               </button>
                            </div>
                         </form>
